3cd9aa7ef48ad08f09f4fc8a04090fc56ad5062f
NasriddUbaydullox <nasriddinovadildora45@gmail.com>
2024-11-22 09:39:06 +0500

    New File Created

---
+++ GitHubForTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubForTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world 1");
+        }
+    }
+}
