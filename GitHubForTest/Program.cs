7f495cb67a25ee7277f1b0ca0777226552de8fbb
NasriddUbaydullox <nasriddinovadildora45@gmail.com>
2024-11-22 09:55:56 +0500

    New Code for dev/paintes-registration

--- GitHubForTest/Program.cs
+++ GitHubForTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitHubForTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world 1");
+            Console.WriteLine("for dev/patients-registration commit 1");
         }
     }
 }
