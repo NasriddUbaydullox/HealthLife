abe93d05d22c1be3f6b4210864c8ffd527df947a
NasriddUbaydullox <nasriddinovadildora45@gmail.com>
2024-11-26 14:43:04 +0500

    User and Password 2

--- GitHubForTest/Program.cs
+++ GitHubForTest/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
-
-namespace GitHubForTest
+﻿namespace Exeption_Handling
 {
     internal class Program
     {
+        public static string orgUserName = "Admin";
+        public static string orgPassword = "Password";
         static void Main(string[] args)
         {
-            Console.WriteLine("for dev/patients-registration Commit !!!!");
+            Console.Write("Username: ");
+            string userName = Console.ReadLine();
+            Console.Write("\nPassword: ");
+            string password = Console.ReadLine();
+            try
+            {
+                ValidateUser(userName, password);
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                UserException a = new UserException();
+                Console.WriteLine(a.Message);
+                
+            }
+            finally
+            {
+
+                Console.WriteLine("Session finished ");
+
+            }
+        }
+        public static void ValidateUser(string userName, string password)
+        {
+            if (password != orgPassword || userName != orgUserName)
+            {
+                Logger.LogError(userName, password);
+                Console.WriteLine("Username or password incorrect!");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Welcome to the system...");
+            }
+        }
+        public class UserException : Exception
+        {
+            public const string Code = "invalid_user_credentials";
+        }
+        public class Logger
+        {
+            public static void LogError(string message, string code)
+            {
+                DateTime dateTime = DateTime.Now;
+                Console.WriteLine("[" + dateTime +" ERR" + "]");
+            }
         }
     }
 }
